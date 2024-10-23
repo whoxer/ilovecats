ab0fcb6af169964b538b359e909951e0baef4287
whoxer <natanaelvsant@gmail.com>
2024-10-23 17:07:05 -0300

    Nova tela e estilização correspondente

---
+++ src/pages/WebCats.tsx
@@ -0,0 +1,12 @@
+import "./WebCats.css"
+import Bar from "../layout/Bar"
+
+const WebCats = () => {
+    return (
+      <div className="screen">
+        <Bar />
+      </div>
+    );
+}
+
+export default WebCats
