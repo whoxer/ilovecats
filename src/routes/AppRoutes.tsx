1b9e918fceb8564048ad25aca0d39c8b690f7a4e
whoxer <natanaelvsant@gmail.com>
2024-10-23 19:12:04 -0300

    Routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -7,7 +7,7 @@ const AppRoutes = () => {
   return ( //this function will return all the routes and export it so they can be placed on the Browser Router on the main file
     <Routes> 
       <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/webcats" element={<WebCats></WebCats>}></Route>
+      {/* <Route path="/webcats" element={<WebCats></WebCats>}></Route> */}
     </Routes>
   );
 };
