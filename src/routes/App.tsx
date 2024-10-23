1579ccffa38757319205160e8c1cb5d61e72cbab
whoxer <natanaelvsant@gmail.com>
2024-10-23 16:41:29 -0300

    Arquivos movidos de lugar

---
+++ src/routes/App.tsx
@@ -0,0 +1,16 @@
+import {BrowserRouter} from "react-router-dom";
+import AppRoutes from './AppRoutes';
+
+
+function App() {
+  return (
+    <div> 
+       <BrowserRouter>
+          <AppRoutes />
+      </BrowserRouter>
+    </div>
+   
+  );
+}
+
+export default App;
